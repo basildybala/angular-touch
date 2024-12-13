7158096c470c5a68dcc313c09ae31564313ff0ff
basildybala <akbasil123@gmail.com>
2024-12-13 14:22:21 +0530

    angular tocu up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,47 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ApiCallService } from './api-call.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-touch';
-  data={
-    name:"Harshid",
-    place:"Malappuram",
-    age:27
-  }
+  constructor(public apiService: ApiCallService, private http: HttpClient) {}
 
-   dataOFArray=[
-    {name:"harshid",age:12,place:"ard"},
-    {name:"dulqer",age:15,place:"calicut"},
-    {name:"mammuty",age:20,place:"ekm"},
-    {name:"mohanlal",age:11,place:"tvm"},
-  ]
-  public name="HARSHID BASIL"
-  public Str=0
-  increament() {
-    this.Str=++this.Str
-    console.log(this.Str)
+  ngOnInit() {
+    console.log('CALLED dNG ON INIT', this.apiService.getPost().then(a => console.log(a)));
   }
-  decreament(){
-    if(this.Str ===0) return
-    this.Str=-- this.Str
+  title = 'angular-touch';
+  data = {
+    name: 'Harshid',
+    place: 'Malappuram',
+    age: 27,
+  };
+
+  dataOFArray = [
+    { name: 'harshid', age: 12, place: 'ard' },
+    { name: 'dulqer', age: 15, place: 'calicut' },
+    { name: 'mammuty', age: 20, place: 'ekm' },
+    { name: 'mohanlal', age: 11, place: 'tvm' },
+  ];
+  public name = 'HARSHID BASIL';
+  public Str = 0;
+  increament() {
+    this.Str = ++this.Str;
+    this.http
+      .get<any>('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe((res: any) => {
+        console.log(res);
+      });
+    console.log(this.Str);
+  }
+  decreament() {
+    if (this.Str === 0) return;
+    this.Str = --this.Str;
   }
 }
---
+++ src/app/contac-us/contac-us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contac-us',
+  imports: [],
+  templateUrl: './contac-us.component.html',
+  styleUrl: './contac-us.component.css'
+})
+export class ContacUsComponent {
+
+}
